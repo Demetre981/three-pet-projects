6b69cb7263136369d69cd703a72bcd920e9651ca
demetre.87 <113709812+Demetre981@users.noreply.github.com>
2023-01-09 20:38:49 +0200

    Nothing added just check

--- first_program.py
+++ first_program.py
@@ -1,7 +1,7 @@
 import moviepy.editor
 from pathlib import Path
 
-video_file = Path('my_video2.mp4')# first or second video
+video_file = Path('my_video2.mp4')# first or second video))
 
 video = moviepy.editor.VideoFileClip(f"{video_file}")
 audio = video.audio
